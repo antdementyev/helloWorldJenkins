53742786414809dfacceb21569486a84af903449
Anton Dementyev <dementyev@subshell.com>
2016-03-07 14:38:13 +0100

    have changed

--- src/main/java/jenkins/HelloWorld.java
+++ src/main/java/jenkins/HelloWorld.java
@@ -2,8 +2,14 @@ package jenkins;
 
 public class HelloWorld {
 
+	private int unsinn;
+
 	public static void main (String[] args){
 		System.out.println("Hello World!");
 	}
 	
+	public int give1(){
+		return 1;
+	}
+
 }
