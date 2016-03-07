abbefa96bfdb0bb086d20cfeec5e53d8c566dbb2
Anton Dementyev <dementyev@subshell.com>
2016-03-07 09:36:23 +0100

    HelloWorlsd is ready

---
+++ src/test/java/jenkins/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testMain(){
+		assertNotNull("it doesn't matter to have smthg hier");
+	}
+	
+}
